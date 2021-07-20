8be3fc65e3ece85a78c359c69718b4c131f7ba4e
Mathias Radica <radicamathias@gmail.com>
2021-07-20 13:25:32 +0200

    mostly accordion and tabs changes

--- public/js/product-accordions.js
+++ public/js/product-accordions.js
@@ -1,15 +1,25 @@
-let productAcc = document.getElementsByClassName('product_accordion_button');
-let productPanels = document.getElementsByClassName('product_panel');
+// Product accordion and tabs
+
+let productAcc = document.getElementsByClassName('product_accordion_button')
+let productPanels = document.getElementsByClassName('product_panel')
+let tabs = document.getElementsByClassName('tab')
 
 for (let i = 0; i < productAcc.length; i++) {
-  productAcc[i].addEventListener('click', function () {
+  productAcc[i].addEventListener('click', function() {
+    
+    let tabList = document.getElementById('tab-list')
+    tabList.className = ''
 
     for (let i = 0; i < productAcc.length; i++) {
-
+      
       if (this !== productAcc[i]) {
+        
+        tabs[i].classList.add('d-none')
         productPanels[i].style.maxHeight = null
         productAcc[i].classList.remove('open')
       } else {
+        tabList.classList.add('tab' + productAcc[i].id.substring('product-accordion-button'.length))
+        document.getElementById('tab' + productAcc[i].id.substring('product-accordion-button'.length)).classList.remove('d-none')
         productPanels[i].style.maxHeight = productPanels[i].scrollHeight + "px"
         this.classList.add('open')
       }
@@ -17,31 +27,43 @@ for (let i = 0; i < productAcc.length; i++) {
   })
 }
 
-let productFeaturesAcc = document.getElementsByClassName("product_features_accordion_button")[0]
+// Product features accordion
 
-if (productFeaturesAcc) {
-  productFeaturesAcc.addEventListener("click", function () {
+let productFeaturesAcc = document.getElementsByClassName('product_features_accordion_button')
+
+for (let i = 0; i < productFeaturesAcc.length; i++) {
+  productFeaturesAcc[i].addEventListener('click', (function () {
     this.classList.toggle("open")
-    let panel = this.nextElementSibling
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null
+
+    let productPanel1 = this.parentElement.parentElement
+    let productFeaturesPanel1 = this.nextElementSibling
+    let productPanel2 = productFeaturesAcc[(i + 1) % 1].parentElement.parentElement
+    let productFeaturesPanel2 = productFeaturesAcc[(i + 1) % 1].nextElementSibling
+
+    if (productFeaturesPanel1.style.maxHeight) {
+      productFeaturesPanel1.style.maxHeight = null
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "px"
+      if ($(".product-features-table").html() === "") {
+        $(".product-features-spinner-border").removeClass("d-none")
+        let url = window.location.pathname.split("/").pop()
+
+        $.get("api/products/" + url, {}, null, 'json')
+          .then(function (data) {
+            $(".product-features-spinner-border").addClass("d-none")
+            $.each(data.features, function (key, value) {
+              $(".product-features-table").html(`<tr><td>${key}</td><td>${value}</td></tr>`)
+            })
+            productFeaturesPanel1.style.maxHeight = productFeaturesPanel1.scrollHeight + "px"
+            productPanel1.style.maxHeight = productFeaturesPanel1.scrollHeight + productPanel1.scrollHeight + "px"
+            productFeaturesPanel2.style.maxHeight = productFeaturesPanel1.scrollHeight + "px"
+            productPanel2.style.maxHeight = productFeaturesPanel1.scrollHeight + productPanel1.scrollHeight + "px"
+          }, null, null)
+      }
+      productFeaturesPanel1.style.maxHeight = productFeaturesPanel1.scrollHeight + "px"
+      productPanel1.style.maxHeight = productFeaturesPanel1.scrollHeight + productPanel1.scrollHeight + "px"
+      productFeaturesPanel2.style.maxHeight = productFeaturesPanel1.scrollHeight + "px"
+      productPanel2.style.maxHeight = productFeaturesPanel1.scrollHeight + productPanel1.scrollHeight + "px"
     }
   })
-
-  let productFeatures2Acc = document.getElementsByClassName("product_features_2_accordion_button")[0]
-
-  productFeatures2Acc.addEventListener("click", function () {
-    this.classList.toggle("open")
-
-    let productPanel = this.parentElement.parentElement
-      let productFeaturesPanel = this.nextElementSibling
-      if (productFeaturesPanel.style.maxHeight) {
-        productFeaturesPanel.style.maxHeight = null
-      } else {
-        productFeaturesPanel.style.maxHeight = productFeaturesPanel.scrollHeight + "px"
-        productPanel.style.maxHeight = productPanel.scrollHeight + productFeaturesPanel.scrollHeight + "px"
-      }
-  })
+  )
 }
--- public/js/tab-list.js
+++ public/js/tab-list.js
@@ -1,11 +1,21 @@
-function toggle(tab){
-    let tabList=document.getElementById('tab-list')
-    tabList.className=""
-    tabList.classList.add(tab)
+function toggle(tab) {
+    let tabList = document.getElementById('tab-list')
+    let productPanels = document.getElementsByClassName('product_panel')
+    let tabs = document.getElementsByClassName('tab')
 
-    let tabs=document.getElementsByClassName('tab')
-    for(i=0; i<tabs.length; i++){
+    tabList.className = ""
+    tabList.classList.add(tab)
+    
+    for (let i = 0; i < tabs.length; i++) {
         tabs[i].classList.add('d-none')
+
+        if (productAcc[i].id !== 'product-accordion-button' + tab.substring('tab'.length)) {
+            productPanels[i].style.maxHeight = null
+            productAcc[i].classList.remove('open')
+        } else {
+            productPanels[i].style.maxHeight = productPanels[i].scrollHeight + "px"
+            productAcc[i].classList.add('open')
+        }
     }
 
     document.getElementById(tab).classList.remove('d-none')
